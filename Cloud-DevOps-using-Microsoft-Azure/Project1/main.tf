bfd6c8f6ca22dcef4322d1244678e1b3ab424a05
bvt3 <70249677+bvt3@users.noreply.github.com>
2023-02-03 18:47:46 +0800

    Update main.tf

--- Cloud-DevOps-using-Microsoft-Azure/Project1/main.tf
+++ Cloud-DevOps-using-Microsoft-Azure/Project1/main.tf
@@ -59,6 +59,7 @@ terraform {
    resource_group_name          = azurerm_resource_group.project1.name
    allocation_method            = "Static"
    domain_name_label            = "prj1fqdn"
+   sku                          = "Standard"
    tags = var.tags
  }
 
@@ -66,11 +67,11 @@ terraform {
    name                = "prj1-lb"
    location            = azurerm_resource_group.project1.location
    resource_group_name = azurerm_resource_group.project1.name
+   sku                 = "Standard"
 
    frontend_ip_configuration {
      name                          = "prj1-lbfrontendip"
      public_ip_address_id          = azurerm_public_ip.project1.id
-     # subnet_id                     = azurerm_subnet.project1.id
      private_ip_address_allocation = "Dynamic"
    }
 
@@ -89,13 +90,13 @@ terraform {
    port                = var.application_port
  }
 
- resource "azurerm_lb_rule" "lbnatrule" {
+ resource "azurerm_lb_rule" "project1" {
    resource_group_name            = azurerm_resource_group.project1.name
    loadbalancer_id                = azurerm_lb.project1.id
-   name                           = "ssh-inbound-rule"
+   name                           = "lb-inbound-rule"
    protocol                       = "Tcp"
-   frontend_port                  = var.application_port
-   backend_port                   = var.application_port
+   frontend_port                  = 80
+   backend_port                   = 80
    backend_address_pool_id        = azurerm_lb_backend_address_pool.project1.id
    frontend_ip_configuration_name = "prj1-lbfrontendip"
    probe_id                       = azurerm_lb_probe.project1.id
@@ -131,34 +132,22 @@ terraform {
    name                = "prj1-nsg"
    location            = azurerm_resource_group.project1.location
    resource_group_name = azurerm_resource_group.project1.name
-
-   # Add rule for inbound access
-   # Uncomment to allow SSH access
-   #security_rule {
-   #  name                       = "SSH"
-   #  priority                   = 100
-   #  direction                  = "Inbound"
-   #  access                     = "Allow"
-   #  protocol                   = "Tcp"
-   #  source_port_range          = "*"
-   #  destination_port_range     = var.application_port
-   #  source_address_prefix      = "*"
-   #  destination_address_prefix = "*"
-   #}
-
-   security_rule {
-     name                       = "DenyEverything"
-     priority                   = 105
-     direction                  = "Inbound"
-     access                     = "Deny"
-     protocol                   = "*"
-     source_port_range          = "*"
-     destination_port_range     = "*"
-     source_address_prefix      = "*"
-     destination_address_prefix = "*"
-   }
-
    tags = var.tags
+ }
+
+ resource "azurerm_network_security_rule" "example" {
+   for_each                    = local.nsgrules 
+   name                        = each.value.name
+   direction                   = each.value.direction
+   access                      = each.value.access
+   priority                    = each.value.priority
+   protocol                    = each.value.protocol
+   source_port_range           = each.value.source_port_range
+   destination_port_range      = each.value.destination_port_range
+   source_address_prefix       = each.value.source_address_prefix
+   destination_address_prefix  = each.value.destination_address_prefix
+   resource_group_name         = azurerm_resource_group.project1.name
+   network_security_group_name = azurerm_network_security_group.project1.name
  }
 
  resource "azurerm_subnet_network_security_group_association" "nsg-association" {
@@ -177,7 +166,7 @@ terraform {
    # vm_size               = "Standard_DS1_v2"
 
    delete_os_disk_on_termination = true
-   # delete_data_disks_on_termination = true
+   delete_data_disks_on_termination = true
 
    storage_image_reference {
      id = "${data.azurerm_image.image.id}"
